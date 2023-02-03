98ffbfe8d5f62b1484d64b284693fd726896fa14
Tushar-E <117970573+Tushar-E@users.noreply.github.com>
2023-02-03 21:16:29 +0530

    added some change

--- routes/device_route.js
+++ routes/device_route.js
@@ -5,9 +5,9 @@ const validator_device = require('../validator/device_validator')
 const controller_device = require('../controller/device_controller')
 
 
-router.post('/create_device',validator_device,controller_device.create_device);
+router.post('/create',validator_device,controller_device.create_device);
 
-router.get('/view_device',validator_device,controller_device.view_device);
+router.get('/view',validator_device,controller_device.view_device);
 
 
 module.exports=router;
