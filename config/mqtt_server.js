638e6d5ae019c3561c0f294b248c68535c674036
Tushar-E <117970573+Tushar-E@users.noreply.github.com>
2023-02-05 20:18:53 +0530

    -u

---
+++ config/mqtt_server.js
@@ -0,0 +1,18 @@
+const mqtt = require('mqtt')
+
+
+const host = 'broker.emqx.io'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+
+const connectUrl = `mqtt://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
